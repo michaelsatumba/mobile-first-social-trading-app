2b953816681bbbbc7fbd9e629a7a46484a92eb66
michaelsatumba <mjsatumba@gmail.com>
2024-09-10 12:18:54 -0700

    change user img to icon

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const User = ({ theme }) => {
   return (
     <div className="flex flex-col items-center p-4">
-      <img
-        src="https://via.placeholder.com/150"
-        alt="User Logo"
-        className="rounded-full w-32 h-32 mb-4"
-      />
+      <FontAwesomeIcon icon={faUser} size="3x"/>
       <h2 className="text-2xl font-bold mb-2">@User3</h2>
       <p className="text-lg">$10,000.96</p>
     </div>
